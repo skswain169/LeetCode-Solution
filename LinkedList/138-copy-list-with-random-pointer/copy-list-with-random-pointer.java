90c09be712861812197ce1ec5657a075de3d2e22
Sanjay Swain <40512886+skswain169@users.noreply.github.com>
2025-11-11 09:40:25 +0530

    Time: 2 ms (1.77%) | Memory: 46.4 MB (8.86%) - LeetSync

---
+++ LinkedList/138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+
+
+    //Copy org list in HM , then traverse HM and assign next,random values
+        Node curr=head;
+        HashMap<Node,Node> nodeMap = new HashMap<>();
+
+        while(curr!=null)
+        {
+            Node copy= new Node(curr.val);
+            nodeMap.put(curr,copy);
+            curr=curr.next;
+        }
+
+        nodeMap.entrySet().stream().
+            forEach(entry -> {
+                     entry.getValue().next = nodeMap.get(entry.getKey().next);
+                     entry.getValue().random = nodeMap.get(entry.getKey().random);
+                    });
+
+    return nodeMap.get(head);
+
+}}
