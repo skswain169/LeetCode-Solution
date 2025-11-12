d3f8e84f2977056390a690ac042831b70affe473
Sanjay Swain <40512886+skswain169@users.noreply.github.com>
2025-11-12 08:52:51 +0530

    Time: 1 ms (100.00%) | Memory: 46.8 MB (7.22%) - LeetSync

--- LinkedList/2-add-two-numbers/add-two-numbers.java
+++ LinkedList/2-add-two-numbers/add-two-numbers.java
@@ -11,38 +11,39 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        ListNode dummy=new ListNode(0);
-        ListNode prev=dummy;
-        int carry=0;
-        while( l1!=null && l2!=null)
-        {
-            int sum= (l1.val+l2.val+carry);
-            int rem= sum%10;             
-            ListNode newNode = new ListNode(rem);
-            carry =  sum>=10 ? sum/10 : 0;
-            prev.next=newNode;
-            prev=newNode;
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = l1 != null ? l1.val : 0;
+            int val2 = l2 != null ? l2.val : 0;
 
-            l1=l1.next ;
-            l2=l2.next ;
+            int sum = (val1 + val2 + carry);
+            int rem = sum % 10;
+            ListNode newNode = new ListNode(rem);
+            carry = sum >= 10 ? sum / 10 : 0;
+            prev.next = newNode;
+            prev = newNode;
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
 
         }
-        ListNode remList = l1!=null ? l1 : (l2!=null) ?  l2 : null; 
+        // ListNode remList = l1!=null ? l1 : (l2!=null) ?  l2 : null; 
 
-            while(remList!=null)
-            {
-                int  sum = remList.val+carry;
-                int rem = sum%10;
-                ListNode newNode= new ListNode(rem);
-                carry =  sum>9 ? sum/10 :0;
-                prev.next=newNode;
-                prev=newNode;
-                remList=remList.next;
-            }
+        //     while(remList!=null)
+        //     {
+        //         int  sum = remList.val+carry;
+        //         int rem = sum%10;
+        //         ListNode newNode= new ListNode(rem);
+        //         carry =  sum>9 ? sum/10 :0;
+        //         prev.next=newNode;
+        //         prev=newNode;
+        //         remList=remList.next;
+        //     }
 
-            if  (carry != 0)
-            prev.next = new ListNode(carry);
-        
-    return dummy.next;
+        //     if  (carry != 0)
+        //     prev.next = new ListNode(carry);
+
+        return dummy.next;
     }
 }
