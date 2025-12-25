ab5390f063667417624ea9f1d8d1cc9233783ac7
Sanjay Swain <40512886+skswain169@users.noreply.github.com>
2025-12-25 15:13:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (5.24%) - LeetSync

--- BinarySearch/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ BinarySearch/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,20 +1,30 @@
 class Solution {
     public int findMin(int[] nums) {
 
-        int l = 0, r = nums.length-1;
+        int l = 0, r = nums.length - 1;
 
         int result = Integer.MAX_VALUE;
 
-        while (l <= r) {
+        while (l <= r) { //index of array
 
             int mid = (l + r) / 2;
 
+            /*if rotated array is at original state after rotation,
+                means 1st element is min , since in ascending order.*/
             if (nums[l] < nums[r])
                 return Math.min(result, nums[l]);
+            /*
+            if mid> then left mean min lies at right after rotation.
+            e.g 4,5,6,0,1,2
+            */
             else if (nums[mid] >= nums[l]) {
                 l = mid + 1;
 
-            } else
+            }
+            /*
+            mid < left mean , min could be in left since at right every element will be greater
+            e.g 7,1,2,4,5,6
+            */else
                 r = mid - 1;
             result = Math.min(result, nums[mid]);
         }
