5a94e17f46baa8d7f0542a12548726618d8d9fb7
Sanjay Swain <40512886+skswain169@users.noreply.github.com>
2026-08-20 10:21:53 +0530

    Time: 11 ms (31.84%) | Memory: 44.1 MB (47.46%) - LeetSync

--- SlidingWindow/567-permutation-in-string/permutation-in-string.java
+++ SlidingWindow/567-permutation-in-string/permutation-in-string.java
@@ -1,45 +1,52 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
 
-        if(s1.length() > s2.length())
-        return false;
+        if (s1.length() > s2.length())
+            return false;
 
-        HashMap<Character, Integer> s1map = new HashMap();
-        HashMap<Character, Integer> s2map = new HashMap();
+        int[] s1count = new int[26];
+        int[] s2count = new int[26];
 
-        for(int i=0; i< s1.length();i++)
-        {
-            s1map.put(s1.charAt(i),s1map.getOrDefault(s1.charAt(i),0)+1);
+        //freq count of first window(size of s1)
+        for (int i = 0; i < s1.length(); i++) {
+            s1count[s1.charAt(i) - 'a']++;
+            s2count[s2.charAt(i) - 'a']++;
         }
 
-        // for(int i=0; i< s2.length();i++)
-        // {
-        //     s2map.put(s2.charAt(i),s2.getOrDefault(0)+1);
-        // }
+        //get initialmatches
+        int j = 0, matches = 0;
+        while (j < 26) {
+            if (s1count[j] == s2count[j])
+                matches++;
 
-        int l=0, r=0;
+            j++;
+        }
 
-        while( r < s2.length())
-        {
+        //slide  window as per s1 size and update matches
+        int l = 0, r = s1.length();
+        while (r < s2.length()) {
 
-            s2map.put(s2.charAt(r),s2map.getOrDefault(s2.charAt(r),0)+1);
+            if (matches == 26)
+                return true;
+            int idx = s2.charAt(r) - 'a';
+            s2count[idx]++;
+            if (s2count[idx] == s1count[idx])
+                matches++;
+            else if (s2count[idx] - 1 == s1count[idx])
+                matches--;
 
-            if(r-l+1 == s1.length())
-            {
-                if (s1map.equals(s2map.entrySet().stream()
-                                    .filter(e -> e.getValue() != 0)
-                                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)))) return true;
+            idx = s2.charAt(l) - 'a';
+            s2count[idx]--;
+            if (s2count[idx] == s1count[idx])
+                matches++;
+            else if (s2count[idx] + 1 == s1count[idx])
+                matches--;
 
-
-                s2map.put(s2.charAt(l),(s2map.get(s2.charAt(l))-1)<0 ? 0 : s2map.get(s2.charAt(l))-1);
-                l++;
-
-            }
-
-
+            l++;
             r++;
-
         }
-        return false;
+
+        return matches == 26;
+
     }
 }
